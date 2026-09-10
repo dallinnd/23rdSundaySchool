6b21801771c3c5d68eb5c900586f829b211563de
dallinnd <156145316+dallinnd@users.noreply.github.com>
2026-09-09 21:09:58 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,9 +16,18 @@ self.addEventListener('install', event => {
     );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    // Deletes all existing caches
+                    return caches.delete(cacheName); 
+                })
+            );
+        })
     );
+    // Forces the new service worker to take control immediately
+    self.clients.claim(); 
 });
+
